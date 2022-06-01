a0f49670bb4c68fc55f72034f6815ff7751c9c1c
sekar.minati <sekar.minati@ocbcnisp.com>
2022-06-01 07:52:44 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home";
+// import Home from "./pages/home";
 import Gallery from "./pages/gallery";
 
 function App() {
